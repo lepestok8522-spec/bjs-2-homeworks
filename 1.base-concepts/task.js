adde1a0ac5d0a5dda5a1ccd8493d80ae9b4af111
Neto test <Lepestok85@bk.ru>
2026-08-11 22:02:21 +0500

    Reformat code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -16,13 +16,6 @@ function solveEquation(a, b, c) {
 		(-b - Math.sqrt(d)) / (2 * a)
 	];
 }
-function testCase() {
-  console.log('Тест 1 (два корня):', solveEquation(1, -3, 2)); // [2, 1]
-  console.log('Тест 2 (один корень):', solveEquation(1, 2, 1)); // [-1]
-  console.log('Тест 3 (нет корней):', solveEquation(1, 0, 1));  // []
-}
-
-testCase();
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	// Преобразуем параметры в числа
