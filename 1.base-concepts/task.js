29a335452609e3b55dc2bc4ea74fcb60278ed204
Neto test <Lepestok85@bk.ru>
2026-08-11 21:26:16 +0500

    task 1 solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return [];
+	}
+
+	if (d === 0) {
+		return [-b / (2 * a)];
+	}
+
+	return [
+		(-b + Math.sqrt(d)) / (2 * a),
+		(-b - Math.sqrt(d)) / (2 * a)
+	];
+}
+function testCase() {
+  console.log('Тест 1 (два корня):', solveEquation(1, -3, 2)); // [2, 1]
+  console.log('Тест 2 (один корень):', solveEquation(1, 2, 1)); // [-1]
+  console.log('Тест 3 (нет корней):', solveEquation(1, 0, 1));  // []
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+testCase();
